c22c926a3396642115d3765973e12248ba5522ef
Jordi Casanella <jordikiter@hotmail.es>
2023-04-01 23:51:04 +0100

    Enable login

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -1,23 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Lesson } from './schema/lesson.schema';
 
 @Injectable()
 export class LessonService {
+    private readonly logger = new Logger(LessonService.name);
     constructor(@InjectModel(Lesson.name) private readonly lessonModel: Model<Lesson>) {}
 
     async create(id: string, name: string, startDate: string, endDate: string): Promise<Lesson> {
+        this.logger.log(`create lesson with id ${id}`);
         const createdLesson = await this.lessonModel.create({ _id: id, name: name, startDate: startDate, endDate: endDate});
         return createdLesson;
     }
 
     async findById(id: string): Promise<Lesson> {
+        this.logger.log(`findById lesson with id ${id}`);
         const lesson = await this.lessonModel.findById(id).exec();
         return lesson;
     }
 
     async getAll(): Promise<Lesson[]> {
+        this.logger.log(`getAll lessons`);
         const lessons = await this.lessonModel.find({});
         return lessons;
     }
